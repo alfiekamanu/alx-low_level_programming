2568ec87195bcbaf5e896ffddc9550c517a8f469
alfiekamanu <alfiekamanu@gmail.com>
2023-02-17 11:01:00 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{ char ch;
+	/* your code goes there */
+	for
+	       (ch = 'a';
+		ch <= 'z';
+		ch++)
+	       {  putchar(ch); }
+	return (0);
+}
