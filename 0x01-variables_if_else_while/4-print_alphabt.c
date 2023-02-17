a354f5b72209bdc4ef98da29142736e9ae0458dd
alfiekamanu <alfiekamanu@gmail.com>
2023-02-17 12:10:18 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{ char ch;
+	/* your code goes there */
+	for
+		(ch = 'a';
+		 ch <= 'z';
+		 ch++)
+		{  putchar(ch); }
+	if
+		(ch != 'e'
+		 && ch != 'q')
+	{ putchar(ch); }
+	putchar('\n');
+	return (0);
+}
