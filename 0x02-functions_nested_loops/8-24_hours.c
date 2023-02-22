a7a4a930e462342eebd29581b269edecda3437d6
alfiekamanu <alfiekamanu@gmail.com>
2023-02-21 23:25:43 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,20 @@
  */
 void jack_bauer(void);
 {
-	int j;
+	int j, b;
 	
-	for (j = 00 : 00; j < 24 : 00; j++)
+	for (j = 0; j < 24; j++)
 	{
-		_putchar (j);
-		_putchar (' ');
-		_putchar ('\n');
+		for (b = 0; b < 60; b++)
+		{
+			_putchar ((j / 10) + 0);
+			_putchar ((j % 10) + 0);
+			_putchar (':');
+			_putchar ((b / 10) + 0);
+			_putchar ((b % 10) + 0);
+			_putchar (' ');
+			_putchar ('\n');
+		}
 	}
 	return (0);
 }
