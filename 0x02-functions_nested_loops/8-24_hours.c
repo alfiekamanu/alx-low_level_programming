63136984ec2a0f77e776e426d7796693cf177c55
alfiekamanu <alfiekamanu@gmail.com>
2023-02-21 22:53:33 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Jack Bauer - function that prints every minute of the day
+ *
+ * Return: Always Successful (0)
+ */
+void jack_bauer(void);
+{
+	int j;
+	
+	for (j = 00 : 00; j < 24 : 00; j++)
+	{
+		_putchar (j);
+		_putchar (' ');
+		_putchar ('\n');
+	}
+	return (0);
+}
