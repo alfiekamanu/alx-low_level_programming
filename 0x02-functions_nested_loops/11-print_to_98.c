a2ba6ccab59dbcfb0874c094d663f82c3817634c
alfiekamanu <alfiekamanu@gmail.com>
2023-02-22 09:31:57 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,2 +1,17 @@
 #include "main.h"
-
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 98; n++)
+	{
+		_putchar (n);
+		_putchar (',');
+		_putchar (' ');
+	}
+	Return (0);
+}
