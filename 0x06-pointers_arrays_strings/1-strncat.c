12a3d47c5ea4cec3a568f852ceebb19810e3ee03
alfiekamanu <alfiekamanu@gmail.com>
2023-03-02 00:04:18 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - function that concantenates two strings
+ * @dest:variable to be concantenated with
+ * @src: variable to be concantenated
+ * Return: Successful 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
+
+
+
