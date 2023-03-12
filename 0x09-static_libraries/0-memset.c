972fe97e193cc2ed91a4da896fe7e4a515a8221f
alfiekamanu <alfiekamanu@gmail.com>
2023-03-12 11:34:44 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * *_memset - a function that fills memory with a constant byte
+ * @b: constant byte to be filled
+ * @s: pointer to character b
+ * @n: the maximum value
+ * Return: Successful 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; n > 0; a++, n--)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,1 +1,28 @@
 #include "main.h"
+/**
+ * _strncat - function that concantenates two strings
+ * @dest:variable to be concantenated with
+ * @src: variable to be concantenated
+ * @n: maximum value of variable
+ * Return: Successful 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
