411e30bdec86f4ad34219bd95c77f146aaeeea05
alfiekamanu <alfiekamanu@gmail.com>
2023-03-22 08:50:56 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_name - function that prints a name
+ * @name: name of the person
+ * @f: is pointer
+ * Return: Always successful 0
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
